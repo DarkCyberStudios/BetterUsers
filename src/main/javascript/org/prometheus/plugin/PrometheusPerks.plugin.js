e0298efa81e19ab618cd682a57806b124207b2e7
NormalBettle437 <conner.rinehart.jetbrains@gmail.com>
2022-01-07 13:33:53 -0600

    Updated PrometheusPerks.plugin.js

--- src/main/javascript/org/prometheus/plugin/PrometheusPerks.plugin.js
+++ src/main/javascript/org/prometheus/plugin/PrometheusPerks.plugin.js
@@ -3,7 +3,7 @@
  * @author NormalBettle437
  * @source https://raw.githubusercontent.com/NormalBettle437/PrometheusPerks/main/src/main/javascript/org/prometheus/plugin/PrometheusPerks.plugin.js
  * @updateUrl https://raw.githubusercontent.com/NormalBettle437/PrometheusPerks/main/src/main/javascript/org/prometheus/plugin/PrometheusPerks.plugin.js
- * @version 1.0.16
+ * @version 1.0.18
  * @description Allows you to locally assign a banner or an avatar of your choosing
  */
 
@@ -29,7 +29,7 @@ module.exports = (() => {
             }],
             "source": "https://raw.githubusercontent.com/NormalBettle437/PrometheusPerks/main/src/main/javascript/org/prometheus/plugin/PrometheusPerks.plugin.js",
             "updateUrl": "https://raw.githubusercontent.com/NormalBettle437/PrometheusPerks/main/src/main/javascript/org/prometheus/plugin/PrometheusPerks.plugin.js",
-            "version": "1.0.16",
+            "version": "1.0.18",
             "description": "Allows you to locally assign a banner or an avatar of your choosing"
         },
         "main": "PrometheusPerks.plugin.js"
@@ -148,6 +148,33 @@ module.exports = (() => {
                 }
 
                 removeAvatar() {
+
+                    clearInterval(this.clientsideAvatar && this.staticClientsideAvatar);
+
+                    ["160", "100"].forEach(sizes => document.querySelectorAll(`[src = "${this.settings.avatarUrl}"]`).forEach(avatar => {
+                                
+                        avatar.src = `https://cdn.discordapp.com/avatars/${ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().id}/${ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().avatar}.webp?size=${sizes}`;
+                        document.querySelectorAll(`.messageListItem-1-jvGY > [data-author-id="${ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().id}"]`).forEach((parent, index) => {
+                                        
+                            const element = document.querySelectorAll(`.messageListItem-1-jvGY > [data-author-id="${ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().id}"] .avatar-1BDn8e`)[index];
+                            element.src = `https://cdn.discordapp.com/avatars/${ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().id}/${ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().avatar}.webp?size=${sizes}`;
+                        });
+                    }));
+
+                    ["56", "40", "32", "20", "10"].forEach(sizes => document.querySelectorAll(`[src = "${this.settings.staticAvatarUrl}"]`).forEach(avatar => {
+                                
+                        avatar.src = `https://cdn.discordapp.com/avatars/${ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().id}/${ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().avatar}.webp?size=${sizes}`;
+                    }));
+
+                    document.querySelectorAll(`.avatarContainer-28iYmV.avatar-3tNQiO.avatarSmall-1PJoGO`).forEach(avatar => {
+                                
+                        avatar.style = `background-image: url("https://cdn.discordapp.com/avatars/${ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().id}/${ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().avatar}.webp?size=${sizes}");`;
+                    });
+
+                    document.querySelectorAll(`.avatarUploaderInner-3UNxY3.avatarUploaderInner-mAGe3e`).forEach(avatar => {
+
+                        avatar.style = `background-image: url("https://cdn.discordapp.com/avatars/${ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().id}/${ZeresPluginLibrary.DiscordModules.UserStore.getCurrentUser().avatar}.webp?size=${sizes}");`;
+                    });
                 }
 
                 removeBanner() {
@@ -183,7 +210,7 @@ module.exports = (() => {
                 setAvatar() {
 
                     PluginUtilities.saveSettings(this.getName(), this.settings);
-                    if (this.settings.clientsideAvatar && this.settings.staticClientsideAvatar && this.settings.avatarUrl && this.settings.staticAvatarUrl) {
+                    if ((this.settings.clientsideAvatar && this.settings.staticClientsideAvatar) && (this.settings.avatarUrl && this.settings.staticAvatarUrl)) {
 
                         this.clientsideAvatar = setInterval(() => {
 
